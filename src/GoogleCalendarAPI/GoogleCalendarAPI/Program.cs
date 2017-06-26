1f5849a8e9dbae0de1cc74e5fe9e57e36eb1a965
rglos <rick.glos@gmail.com>
2017-06-26 10:49:43 -0700

    Add initial console application project with TODO for getting meals from our meal calendar.

---
+++ src/GoogleCalendarAPI/GoogleCalendarAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendarAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TODO: use the google calendar API to get meals from our meal calendar");
+        }
+    }
+}
